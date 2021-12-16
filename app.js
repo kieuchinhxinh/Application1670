e2488babbde8afd8ee40f85bc0098d5c435fa634
khiemtran105 <77436021+khiemtran105@users.noreply.github.com>
2021-12-16 15:59:56 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const session = require('express-session')
 const { checkUserRole } = require('./databaseHandler')
-const { requiresLogin } = require('./dbLib')
+const {
+    requiresLogin,
+    requiresAdmin
+} = require('./dbLib')
+var cookieParser = require('cookie-parser')
 
 const app = express()
 
@@ -10,7 +14,7 @@ app.set('view engine', 'hbs')
 app.use(express.static('public'))
 
 app.use(express.urlencoded({ extended: true }))
-app.use(session({ secret: '124447yd@@$%%#', cookie: { maxAge: 900000 }, saveUninitialized: false, resave: false }))
+app.use(session({ secret: '124447yd@@$%%#', cookie: { maxAge: 60000 }, saveUninitialized: false, resave: false }))
 
 app.get('/', requiresLogin, (req, res) => {
     const user = req.session["User"]
@@ -35,7 +39,7 @@ app.post('/login', async(req, res) => {
             name: name,
             role: role
         }
-        res.redirect('/staffIndex')
+        res.redirect('/staff/staffHome')
 
     } else if (role == "trainee") {
         req.session["trainee"] = {
@@ -51,6 +55,7 @@ app.post('/login', async(req, res) => {
         res.redirect('/trainer')
     }
 })
+
 
 app.get('/login', (req, res) => {
     res.render('login')
@@ -72,6 +77,16 @@ app.use('/admin', adminController)
 
 // const traineeController = require('./controllers/trainee')
 // app.use('/trainee', traineeController)
+
+
+
+// const staffController = require('./staff')
+// app.use('/staff',staffController)
+
+// const trainerController = require('./trainer')
+// app.use('/trainer',trainerController)
+
+
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT)
