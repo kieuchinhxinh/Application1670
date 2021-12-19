0523c6208f4c135f409f83f9c5e2b07164c5e328
NguyenVanDuy0909 <80749076+NguyenVanDuy0909@users.noreply.github.com>
2021-12-20 01:16:37 +0700

    file trainer view trainee

--- controllers/trainer.js
+++ controllers/trainer.js
@@ -26,9 +26,79 @@ router.post('/MarkTrainee',async (req,res)=>{
 router.get('/trainerMarkTrainee',(req,res)=>{
     res.render('trainerMarkTrainee')
 })
-///
+router.get('/trainerProfileUpdate',(req,res)=>{
+    res.render('trainerProfileUpdate')
+})
+//------------------
+
+router.get('/viewTrainee', async(req, res) => {
+    let db = await getDB();
+    let results = await db.collection("trainee").find({}).toArray();
+    res.render('trainerViewTrainee', {
+        trainee : results
+    })
+    
+})
 
 
+router.get('/', (req, res) => {
+    res.render('staff')
+})
+router.get('/addTrainee', (req, res) => {
+        res.render('addTrainee')
+    })
+    //Submit add User
+router.post('/addTrainee', (req, res) => {
+    const name = req.body.txtName
+    const userName = req.body.txtUserName
+    const dateofbirth = req.body.txtDate
+    const education = req.body.txtEducation
+    const email = req.body.txtEmail
+    const newTrainee = {
+        name: name,
+        userName: userName,
+       // age: age,
+        email: email,
+        education: education,
+        dateofbirth: dateofbirth
+    }
+    insertObject("trainee", newTrainee)
+    res.render('staff')
+})
+
+
+router.get('/addCourseCategory', (req, res) => {
+    res.render('addCourseCategory')
+})
+router.post('/addCourseCategory', async(req, res) => {
+    const ccName = req.body.txtName
+    const description = req.body.txtDescription
+    const newCourseCategory = {
+        coursecategoryName: ccName,
+        description: description
+    }
+    await insertObject("coursecategory", newCourseCategory)
+    res.redirect('/staff')
+
+})
+router.get('/addCourse', (req, res) => {
+    res.render('addCourse')
+})
+router.post('/addCourse', async(req, res) => {
+    const name = req.body.txtName
+    const description = req.body.txtDescription
+    const trainerId = req.body.txtTrainerId
+
+    const newCourse = {
+        courseName: name,
+        description: description,
+        trainerId: trainerId,
+
+    }
+    await insertObject("course", newCourse)
+    res.redirect('/staff')
+
+})
 
 
 module.exports = router;
