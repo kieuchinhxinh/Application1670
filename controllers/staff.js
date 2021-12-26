d6867c215d26dc131d59320e56a84ee83594d7b0
kieuchinhxinh <80748999+kieuchinhxinh@users.noreply.github.com>
2021-12-27 06:05:46 +0700

    backend if staff

--- controllers/staff.js
+++ controllers/staff.js
@@ -1,24 +1,28 @@
 const express = require('express')
+const async = require('hbs/lib/async')
 const {
     insertObject,
     getDB,
     deleteTrainee,
     deleteCategory,
-    deleteCourse,ObjectId
+    deleteCourse,
+    ObjectId,
+    UpdateStaff
 } = require('../databaseHandler')
 const {
-    requireStaff
+    requireStaff,
+
 } = require('../dbLib')
 const router = express.Router()
-router.get('/', (req, res) => {
+router.get('/', requireStaff, (req, res) => {
     res.render('staff')
 })
 
-router.get('/addTrainee', (req, res) => {
+router.get('/addTrainee', requireStaff, (req, res) => {
         res.render('addTrainee')
     })
     //Submit add User
-router.post('/addTrainee', (req, res) => {
+router.post('/addTrainee', requireStaff, async(req, res) => {
     const name = req.body.txtName
     const userName = req.body.txtUserName
     const dateofbirth = req.body.txtDate
@@ -27,20 +31,20 @@ router.post('/addTrainee', (req, res) => {
     const newTrainee = {
         name: name,
         userName: userName,
-        age: age,
+
         email: email,
         education: education,
         dateofbirth: dateofbirth
     }
     insertObject("trainee", newTrainee)
-    res.render('/staff')
+    res.render('staff')
 })
 
 
-router.get('/addCourseCategory', (req, res) => {
+router.get('/addCourseCategory', requireStaff, (req, res) => {
     res.render('addCourseCategory')
 })
-router.post('/addCourseCategory', async(req, res) => {
+router.post('/addCourseCategory', requireStaff, async(req, res) => {
     const ccName = req.body.txtName
     const description = req.body.txtDescription
     const newCourseCategory = {
@@ -51,10 +55,10 @@ router.post('/addCourseCategory', async(req, res) => {
     res.redirect('/staff')
 
 })
-router.get('/addCourse', (req, res) => {
+router.get('/addCourse', requireStaff, (req, res) => {
     res.render('addCourse')
 })
-router.post('/addCourse', async(req, res) => {
+router.post('/addCourse', requireStaff, async(req, res) => {
     const name = req.body.txtName
     const courseId = req.body.txtCourseId
     const trainerId = req.body.txtTrainerId
@@ -70,14 +74,14 @@ router.post('/addCourse', async(req, res) => {
 
 })
 
-router.get('/viewTrainee', async(req, res) => {
+router.get('/viewTrainee', requireStaff, async(req, res) => {
     let db = await getDB();
     let results = await db.collection("trainee").find({}).toArray();
     res.render('viewTrainee', {
         trainee: results
     })
 })
-router.get('/viewCourse', async(req, res) => {
+router.get('/viewCourse', requireStaff, async(req, res) => {
     let db = await getDB();
     let results = await db.collection("course").find({}).toArray();
     res.render('viewCourse', {
@@ -85,22 +89,26 @@ router.get('/viewCourse', async(req, res) => {
     })
 })
 
-router.get('/viewStaff', async(req, res) => {
+router.get('/viewStaff', requireStaff, async(req, res) => {
     let db = await getDB();
     let results = await db.collection("staff").find({}).toArray();
     res.render('viewStaff', {
         staff: results
     })
 })
-router.get('/viewCourseCategory', async(req, res) => {
+router.get('/viewCourseCategory', requireStaff, async(req, res) => {
     let db = await getDB();
     let results = await db.collection("coursecategory").find({}).toArray();
     res.render('viewCourseCategory', {
         coursecategory: results
     })
 })
+router.get('/staff', async(req, res) => {
 
-router.get('/viewTrainer', async(req, res) => {
+    res.render('staff')
+})
+
+router.get('/viewTrainer', requireStaff, async(req, res) => {
     let db = await getDB();
     let results = await db.collection("trainer").find({}).toArray();
     res.render('viewTrainer', {
@@ -108,34 +116,120 @@ router.get('/viewTrainer', async(req, res) => {
     })
 })
 
-
-
-router.get('/delete_category', async(req, res) => {
+router.get('/delete_category', requireStaff, async(req, res) => {
     const ccn = req.query.coursecategoryName;
     await deleteCategory(ccn);
     res.redirect('viewCourseCategory')
 })
 
-router.get('/delete_course', async(req, res) => {
+router.get('/delete_course', requireStaff, async(req, res) => {
     const cn = req.query.courseName;
     await deleteCourse(cn);
     res.redirect('viewCourse')
 })
-router.get('/delete_trainee', async(req, res) => {
+router.get('/delete_trainee', requireStaff, async(req, res) => {
     const us = req.query.userName;
     await deleteTrainee(us);
     res.redirect('viewTrainee')
 })
-router.get('/editTrainee', async(req, res) => {
+
+router.get('/staffProfile', requireStaff, async(req, res) => {
+    const user = req.session["staff"]
+    const db = await getDB();
+    const info = await db.collection("staff").findOne({
+        "userName": user.name
+    });
+
+    res.render('staffProfile', {
+        staff: info
+    });
+})
+
+router.get('/staffUpdateProfile', requireStaff, async(req, res) => {
+    const user = req.session["staff"]
+    const db = await getDB();
+    const info = await db.collection("staff").findOne({
+        "userName": user.name
+    });
+
+    res.render('staffUpdateProfile', {
+        staff: info
+    });
+})
+router.post('/staffUpdateProfile', requireStaff, async(req, res) => {
+    const id = req.body.txtId;
+    const name = req.body.txtName;
+    const age = req.body.txtAge;
+    const email = req.body.txtEmail;
+
+    const userName = req.body.txtUserName;
+    const address = req.body.txtAddress;
+    const filter = {
+        _id: ObjectId(id)
+    }
+    const updateToStaffs = {
+        $set: {
+            name: name,
+            age: age,
+            email: email,
+            userName: userName,
+            address: address,
+
+
+        }
+    }
+
+    const db = await getDB();
+    await db.collection('staff').updateOne(filter, updateToStaffs);
+    const st = await db.collection('staff').findOne({
+        _id: ObjectId(id)
+    });
+
+    res.render('staffProfile', {
+        staff: st
+    });
+})
+router.get('/staffEditTrainee', requireStaff, async(req, res) => {
     const id = req.query.id;
 
     const db = await getDB();
-    const e = await db.collection("trainee").findOne({
+    const t = await db.collection("trainee").findOne({
         _id: ObjectId(id)
     });
 
     res.render('staffEditTrainee', {
-        trainee: e
+        trainee: t
     });
 })
+router.post('/staffUpdateTrainee', requireStaff, async(req, res) => {
+    const id = req.body.txtId;
+    const nameInput = req.body.txtName;
+    const emailInput = req.body.txtEmail;
+    const ageInput = req.body.txtAge;
+    const specialtyInput = req.body.txtSpecialty;
+    const addressInput = req.body.txtAddress;
+
+    UpdateTrainee(id, nameInput, emailInput, ageInput, specialtyInput, addressInput);
+
+    res.redirect('viewTrainee');
+})
+router.post('/searchTrainee', requireStaff, async(req, res) => {
+    const searchName = req.body.txtSearch;
+
+    const db = await getDB();
+    const searchTrainee = await db.collection("trainee").find({
+        name: searchName
+    }).toArray();
+
+    res.render('viewTrainee', {
+        data: searchTrainee
+    })
+})
+
+
+
+router.get('/assignTrainer', requireStaff, (req, res) => {
+
+})
+
 module.exports = router;
